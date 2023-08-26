34a1cc1fff18967f9d701b1c63a0a044b8062b48
Wendy Zhang <wendy@mononoke.io>
2023-08-26 06:49:12 -0400

    Update Mononoke contract

--- contracts/genesis-collection/mononoke.sol
+++ contracts/genesis-collection/mononoke.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Mononoke is ERC721, ERC721URIStorage, Ownable {
+contract Mononoke is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -23,27 +22,5 @@ contract Mononoke is ERC721, ERC721URIStorage, Ownable {
             _safeMint(to, newItemId);
             _setTokenURI(newItemId, uris[i]);
         }
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
